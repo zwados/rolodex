991b361521e799f4d2f98bf9409a9aad62a96135
zwados <lukasz.rozwadowski7@gmail.com>
2020-02-29 10:56:35 +0100

    Updates

---
+++ precache-manifest.75e2b394d9784c1935305b522adfa334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05102a979143521f831bba89a3a48d21",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "25cdf76d75a689b16df4",
+    "url": "/rolodex/static/css/main.ad8a9272.chunk.css"
+  },
+  {
+    "revision": "aed8a5d04430074233fe",
+    "url": "/rolodex/static/js/2.ae0e5de5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rolodex/static/js/2.ae0e5de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25cdf76d75a689b16df4",
+    "url": "/rolodex/static/js/main.31d291e2.chunk.js"
+  },
+  {
+    "revision": "094e9f5e1ceb2ef61815",
+    "url": "/rolodex/static/js/runtime-main.97530f9a.js"
+  }
+]);
