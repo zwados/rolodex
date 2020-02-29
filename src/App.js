e08223445a52aa8f9dd617c8134abdcaa1e54ec2
zwados <lukasz.rozwadowski7@gmail.com>
2020-02-29 10:40:20 +0100

    background and logo added

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
